ff01026ed66e98ed953c4edf1261416ec96bd22a
Anusree <anusreee.p.shaji@gmail.com>
2025-10-08 13:47:13 +0530

    Add requirements.txt for Render deployment

--- pet_adaption/settings.py
+++ pet_adaption/settings.py
@@ -76,15 +76,18 @@ WSGI_APPLICATION = 'pet_adaption.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pet_adp',
-        'USER':'root',
-        'PASSWORD':'anusree@9048',
-        'HOST':'localhost',
-        'PORT':3306,
-
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT", "3306"),
+        'OPTIONS': {
+            'ssl': {'ca': BASE_DIR / 'certs/ca.pem'},
+        },
     }
 }
 
